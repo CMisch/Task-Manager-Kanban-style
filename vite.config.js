0b17e23bfd2a0ead5041cd96343101c2fc2ad108
CMisch <71983546+CMisch@users.noreply.github.com>
2026-02-17 15:51:34 -0800

    Update base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Replace 'cybersec-task-manager' with your exact GitHub repo name
+  // Replace base with your exact GitHub repo name
   base: '/Task-Manager-Kanban-style/',
 })
